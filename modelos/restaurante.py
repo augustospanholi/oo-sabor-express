8c7d829913b04b075eff87f9a2c4c5077c4d0386
Augusto Spanholi <augustospanholi@protonmail.com>
2026-03-19 15:32:05 -0300

    Add Restaurant class with OOP implementation and PyCharm project config

---
+++ modelos/restaurante.py
@@ -0,0 +1,27 @@
+class Restaurante:
+
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+
+    def listar_restaurantes():
+
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | Status')
+
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+restaurante_madero = Restaurante("Madero", "Hamburgueria")
+restaurante_yume = Restaurante("Yume", "Japonesa")
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza Express", "Italiana")
+
+Restaurante.listar_restaurantes()
